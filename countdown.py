bce3695588a0662677087346f6d0cbdf60e752b7
atul <atulbhai.nitin@gmail.com>
2020-12-31 14:26:17 -0500

    time:2:26 date:12/31/2020

--- countdown.py
+++ countdown.py
@@ -9,6 +9,7 @@ while True:
         month = current_time.strftime('%B')
         day = current_time.strftime('%d')
         other_time = current_time.strftime('%X')
+        print('\n'*10)
         print(f'The year is still {int(countdown_year) - 1}, the month is {month}, the day number is {day}, and the time is {other_time}')
         sleep(1)
 print('Happy New Year!🥳')
