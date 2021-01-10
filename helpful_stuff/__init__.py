e68beb430fcc61bedb0b6ef65477623a119252b8
atul <atulbhai.nitin@gmail.com>
2021-01-09 22:19:08 -0500

    time: 10:19 date:1/9/2021

--- helpful_stuff/__init__.py
+++ helpful_stuff/__init__.py
@@ -105,9 +105,26 @@ class Computer:
             return 'no'
 def sum2(*numbers):
     total = 0
-
     for i in numbers:
         if isinstance(i, int):
             return 'you should only put numbers in this function'
         total += i
     return total
+def num_input(prompt=''):
+    value = input(prompt)
+    try:
+        show = int(value)
+    except ValueError:
+        try:
+            show = float(value)
+        except ValueError:
+            raise TypeError('You must enter a number')
+    return show
+def make_copy(origin_file_location, copyfile_location):
+    file = open(origin_file_location, 'r')
+    info = file.read()
+    file.close()
+    file = open(copyfile_location, 'w')
+    file.write(info)
+    file.close()
+    return copyfile_location
