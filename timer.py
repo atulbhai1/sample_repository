f85c0167797230ba51b5060d8f8dbc6dc39be3e2
atul <atulbhai.nitin@gmail.com>
2021-01-16 18:42:59 -0500

    time: 12:44 date:1/16/2021

--- timer.py
+++ timer.py
@@ -1,40 +1,15 @@
-#!/usr/bin/env python3
-from time import *
-from tkinter import *
-
-
-def timer():
-    hours = 3600 * int(input('How many hours?'))
-    minutes = 60 * int(input('How many minutes?'))
-    seconds = int(input('How many seconds?'))
-    length = hours + minutes + seconds
-    then = time()
-    now = time()
-    while now - then < length:
-        now = time()
-        time_left = now - then - length
-        if int(time_left) % 3600 == 0:
-            print(time_left/3600, 'hours left')
-        elif int(time_left) % 60 == 0:
-            print(time_left/60, 'minutes left')
-        else:
-            print(time_left, 'seconds left')
-        sleep(.9937)
-    box = Tk()
-
-    def ok():
-        box.destroy()
-    box.geometry('400x400+0+0')
-    box.iconify()
-    box.update()
-    box.deiconify()
-    box.eval('tk::PlaceWindow . center')
-    box.config(bg='white')
-    message = Label(box, text='Your Timer Is Over!')
-    message.pack()
-    button = Button(box, text='ok', command=ok, bg='blue')
-    button.pack()
-    mainloop()
-
-
-timer()
+import time, tkinter
+hours = int(input('How many Hours?'))
+minutes = int(input('How many Minutes?'))
+seconds = int(input('How many Seconds?'))
+seconds += minutes*60 + hours*3600
+start = time.time()
+keep_counting = True
+while keep_counting:
+    current_time = time.time()
+    if current_time - start >= seconds:
+        keep_counting = False
+window = tkinter.Tk()
+tkinter.Label(window, text='YOUR TIMER IS OVER!').pack()
+tkinter.Button(window, text='OK', command=lambda : window.destroy()).pack()
+window.mainloop()
