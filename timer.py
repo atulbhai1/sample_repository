1eaa83cb59449fe5d83dc290ed0d9a42c3ee1058
atul <atulbhai.nitin@gmail.com>
2021-01-18 11:33:40 -0500

    date:1/18/2021

--- timer.py
+++ timer.py
@@ -6,9 +6,11 @@ seconds += minutes*60 + hours*3600
 start = time.time()
 keep_counting = True
 while keep_counting:
+    time.sleep(1)
     current_time = time.time()
     if current_time - start >= seconds:
         keep_counting = False
+    print(current_time-start)
 window = tkinter.Tk()
 tkinter.Label(window, text='YOUR TIMER IS OVER!').pack()
 tkinter.Button(window, text='OK', command=lambda : window.destroy()).pack()
