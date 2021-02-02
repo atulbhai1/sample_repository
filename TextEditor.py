b144fad80085ed68e84c91f2acae849986838ad2
atul <atulbhai.nitin@gmail.com>
2021-02-02 18:46:45 -0500

    date:2/2/21

--- TextEditor.py
+++ TextEditor.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter.filedialog import asksaveasfilename
 from tkinter.messagebox import askquestion
 from tkinter import ttk
+from tkinter.filedialog import askopenfilename
 window = Tk()
 window.title('My Text Editor')
 Label(window, text='Enter the text below').grid(column=0, row=0)
@@ -12,19 +13,29 @@ scroll = ttk.Scrollbar(window, orient=VERTICAL, command=text_editor.yview)
 scroll.grid(row=1, column=1, sticky=N+S)
 text_editor.config(yscrollcommand=scroll.set)
 def save():
-    result = text_editor.get(1.0, END)
-    with open(asksaveasfilename(), 'w') as file:
-        file.write(result)
+    reply = askquestion('Are You Sure?', 'Are You Sure This Will Create A New File')
+    if reply == 'yes':
+        result = text_editor.get(1.0, END)
+        with open(asksaveasfilename(), 'w') as file:
+            file.write(result)
 def delete():
-    reply = askquestion('Are You Sure?', 'Are You Sure')
+    reply = askquestion('Are You Sure?', 'Are You Sure This Will Erase The Text Editor')
     if reply == 'yes':
         text_editor.delete(0.0, END)
 def close():
     reply = askquestion('Are You Sure?', 'Are You Sure?')
     if reply == 'yes':
         window.destroy()
+def port():
+    cont = askquestion('Are You Sure?', 'Are You Sure This Will Erase The Text Editor')
+    if cont == 'yes':
+        text_editor.delete(0.0, END)
+        with open(askopenfilename(), 'r') as txtFile:
+            text_editor.insert(INSERT, txtFile.read())
 Button(window, text='Save', command=save).grid(row=55, column=0, sticky=W)
 Button(window, text='Clear Text', command=delete).grid(row=55, column=0, sticky=E)
-Button(window, text='Close', command=close).grid(row=55, column=0, sticky=N)
-window.geometry('570x390')
+Button(window, text='Close', command=close).grid(row=56, column=0, sticky=W)
+Button(window, text='Port from other file', command=port).grid(row=56, column=0, sticky=E)
+window.geometry('570x415')
+
 window.mainloop()
