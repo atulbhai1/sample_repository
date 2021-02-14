9aeb11cb0784baf74d57f63c71a8ddb5de38db74
atul <atulbhai.nitin@gmail.com>
2021-02-13 20:23:23 -0500

    changes: added lots of math tests

---
+++ divisiontest.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from random import randint
+score = 0
+for i in range(20):
+    window = Tk()
+    a = randint(0, 100)
+    b = randint(0, 10)
+    while a < b:
+        a = randint(0, 100)
+        b = randint(0, 10)
+    Label(window, text=f'{a} ÷ {b} = ?').pack()
+    entry = Entry(window)
+    entry.pack()
+    def check():
+        global score
+        if a % b == 0:
+            if int(entry.get()) == a//b:
+                score += 1
+        elif entry.get() == f'{a//b} R {a % b}':
+            score += 1
+        window.destroy()
+    Button(window, text='Check', command=check).pack()
+    window.mainloop()
+score = f' You got {score/20}%'
+window = Tk()
+Label(window, text=score).pack()
+Button(window, text='Close', command=window.destroy).pack()
+window.mainloop()
---
+++ subtractiontest.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from random import randint
+score = 0
+for i in range(20):
+    window = Tk()
+    a = randint(0, 1000000)
+    b = randint(0, 1000000)
+    while a < b:
+        a = randint(0, 1000000)
+        b = randint(0, 1000000)
+    Label(window, text=f'{a} - {b} = ?').pack()
+    entry = Entry(window)
+    entry.pack()
+    def check():
+        global score
+        if int(entry.get()) == a - b:
+            score += 1
+        window.destroy()
+    Button(window, text='Check', command=check).pack()
+    window.mainloop()
+score = f' You got {score/20}%'
+window = Tk()
+Label(window, text=score).pack()
+Button(window, text='Close', command=window.destroy).pack()
+window.mainloop()
