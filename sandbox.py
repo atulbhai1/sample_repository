e3060a9ef7a17b027c8f917485371d8b6b5e0d51
atul <atulbhai.nitin@gmail.com>
2021-02-26 19:29:23 -0500

    changes: fixed error in sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -26,7 +26,7 @@ def update():
     elif held_keys['4']:
         block_pick = 4
         sideBlock.change(dirt_texture)
-    if held_keys['backspace']: app.destroy()
+    if held_keys['backspace']: app.destroy(), quit()
 class Block(Button):
     def __init__(self, position=(0, 0, 0), texture=grass_texture):
         super(Block, self).__init__(parent=scene, position=position, model='block.obj'
