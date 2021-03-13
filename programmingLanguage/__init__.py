b64bb1f2657a43a0773580f386b8f8ef11568c98
atul <atulbhai.nitin@gmail.com>
2021-03-12 22:05:06 -0500

    changes: new pass capabilities, variables now work and comparisons are getting started up credit to CodePulse's youtube channel

--- programmingLanguage/__init__.py
+++ programmingLanguage/__init__.py
@@ -4,7 +4,7 @@ def shell():
     text = ''
     while text != 'quit':
         text = input('basic > ')
-        if text == 'quit':continue
+        if text == 'quit'or text == 'pass':continue
         result, error = basic.run('<main>', text)
         if error:
             print(fg.da_red + error.as_string() + fg.rs)
