a218dc65c90a8286b519e21b087c68c07573eeb0
atul <atulbhai.nitin@gmail.com>
2021-03-18 19:18:36 -0400

    changes: inbuilt functions completed!

--- programmingLanguage/__init__.py
+++ programmingLanguage/__init__.py
@@ -1,5 +1,7 @@
-import basic
+import basic, sys
 from sty import fg
+if not sys.stdin.isatty():
+    print(str(fg.da_red + 'Use terminal for access to clear function' + fg.rs))
 def shell(comm):
 
     result, error = basic.run('<main>', text)
@@ -12,3 +14,19 @@ while True:
     if error:print(error)
     elif result:
         print(result)
+#        def execute_terminal(self, exec_ctx):
+#         comm = exec_ctx.symbol_table.get('comm')
+#         if isinstance(comm, String):
+#             d = os.system(str(comm))
+#             if d:
+#                 return RTResult().success(String(str(d)))
+#             return RTResult().success(String('Command Executed Successfully!'))
+#         else:
+#             return RTResult().failure(RTError(self.pos_start, self.pos_end, 'System commands have to be in String format', String('String Needed')))
+#     execute_terminal.arg_names = ['comm']
+#
+#
+#BuiltInFunction.terminal = BuiltInFunction('terminal')
+#
+#
+#global_symbol_table.set('TERMINAL', BuiltInFunction.terminal)
