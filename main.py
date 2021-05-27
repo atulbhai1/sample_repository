f85b008ee2325a570b00ce7806ad7ac196c59f45
atul <atulbhai.nitin@gmail.com>
2021-05-27 19:41:15 -0400

    changes:things

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import pygame
+from tkinter import *
+from google_currency import *
+ops = list(CODES.keys())
+window = Tk()
+product = Label(window, text='You have not done any conversions yet.')
+product.grid(row=0, column=0)
+fromThing = None # should finish!!!
+
