2c48f7bb2242feb532443fd4908327ed2357f32f
atul <atulbhai.nitin@gmail.com>
2021-05-28 21:31:06 -0400

    changes:things

--- main.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-from google_currency import *
-ops = list(CODES.keys())
-window = Tk()
-product = Label(window, text='You have not done any conversions yet.')
-product.grid(row=0, column=0)
-fromThing = None # should finish!!!
-
