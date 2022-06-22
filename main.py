97b892bc4a0a81ced466c40c582181051b2542e8
atulbhai1 <atulbhai.nitin@gmail.com>
2022-06-22 16:22:39 -0300

    changes

---
+++ main.py
@@ -0,0 +1,12 @@
+"""from googletrans import Translator
+
+translator = Translator()
+
+translated_text = translator.translate('안녕하세요.')
+print(translated_text.text)
+
+translated_text = translator.translate('안녕하세요.', dest='ja')
+print(translated_text.text)
+
+translated_text = translator.translate('veritas lux mea', src='la')
+print(translated_text.text)"""
--- youtubeVideoDownloader.py
+++ youtubeVideoDownloader.py
@@ -5,5 +5,6 @@ from tkinter.messagebox import showinfo
 video_link = get_entry('Enter the video link(example: https://youtu.be/ycJe_fhkOuw):', 'Youtube Video Downloader', 'Next')
 os.chdir(askdirectory(initialdir=os.getcwd(), title='Youtube Video Downloader'))
 showinfo('Youtube Video Downloader', 'This might take a while')
-os.system(f'youtube-dl --no-check-certificate {video_link}')
+pr = os.system(f'youtube-dl --no-check-certificate {video_link}')
+print(pr)
 showinfo('Youtube Video Downloader', 'Done!')
